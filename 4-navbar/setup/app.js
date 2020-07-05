3f0a5cf7d966cee1b034e0818273b78ae882dc4f
ankitkrgiri <peace.247ankit@gmail.com>
2020-07-05 07:06:24 +0530

    project 4 completed

--- 4-navbar/setup/app.js
+++ 4-navbar/setup/app.js
@@ -3,3 +3,18 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+
+//select items
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('onkeydown', function () {
+    // if (links.classList.contains('show-links')){
+    //     links.classList.remove('show-links');
+    // }
+    // else{
+    //     links.classList.add('show-links');
+    // }
+    links.classList.toggle('show-links');
+});
